3b9e475ca059ee8d68e487160c4a97b45b1702b3
itsxav <xavierturambi2@gmail.com>
2023-04-29 01:17:37 +0800

    First Commit

---
+++ app/src/main/java/com/itsxav/myrecycleview2/Hero.kt
@@ -0,0 +1,11 @@
+package com.itsxav.myrecycleview2
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Hero(
+    val name: String,
+    val description: String,
+    val photo: Int
+) : Parcelable
